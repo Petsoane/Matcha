2f18923d8adc71aca88bb033e87b2308f58436cf
Lebogang PETSOANE <lpetsoan@c5r5s5.wethinkcode.co.za>
2019-11-28 13:43:14 +0200

    Done adding the users update information.

--- matcha/models.py
+++ matcha/models.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 class DB:
 
@@ -6,6 +7,7 @@ class DB:
 		client = MongoClient()
 		db = client['matcha']
 		self.__users = db['users']
+		self.__posts = db['posts']
 	
 	def get_user(self, query):
 		''' This function will get a single users information'''
@@ -28,3 +30,28 @@ class DB:
 			if key == '_id':
 				continue
 			self.__users.update_one({'_id' : user_id}, {'$set': { key: value}})
+
+
+	# Add a post to the posts table
+	def add_post(self, post):
+		self.__posts.insert_one(post)
+
+	# Get all the posts from the table.
+	def get_posts(self):
+		return self.__posts.find()
+
+	# Get a single post
+	def get_post(self, post_id):
+		post_id = ObjectId(post_id)
+		print(post_id)
+
+		return self.__posts.find_one({'_id': post_id})
+
+
+	# Update a single post.
+	def update_post(self, post):
+		self.__posts.update_one({'_id' : post['_id']}, {'$set': post})
+
+	# delete a single entry
+	def delete_post(self, post):
+		self.__posts.delete_one({'_id': post['_id']})
--- matcha/routes.py
+++ matcha/routes.py
@@ -1,5 +1,6 @@
 from matcha import db, socket, app
 from functools import wraps
+from datetime import datetime
 from flask import render_template, redirect, request, abort, url_for, flash, session
 import os, secrets
 from werkzeug import secure_filename
@@ -19,6 +20,8 @@ def save_picture(form_pic):
     i.save(pic_path)
     return pic_fn
 
+
+
 def login_required(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
@@ -32,7 +35,9 @@ def login_required(f):
 # Create the route for the home page
 @app.route('/')
 def home():
-    return render_template('home.html', logged_in=session.get('username'))
+    posts = db.get_posts()
+    print(posts)
+    return render_template('home.html', logged_in=session.get('username'), posts=posts)
 
 @app.route('/users')
 def users():
@@ -113,6 +118,13 @@ def login():
     return render_template('login.html', details=details)
 
 
+# Route for the logout
+@app.route('/logout')
+@login_required
+def logout():
+    session.pop('username')
+    return redirect( url_for('home') )
+
 
 # Route for the profile page.
 @app.route('/profile', methods=['GET', 'POST'])
@@ -154,3 +166,61 @@ def profile():
                 flash(error, 'danger')
 
     return render_template('profile.html', logged_in=session.get('username'), user=user)
+
+
+# Route for the user posts
+@app.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def new_post():
+    user = db.get_user({'username':session.get('username')})
+    post = {
+        'author': user,
+        'title': '',
+        'content': '',
+        'date_posted': ''
+    }
+
+    if request.method == 'POST':
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+        post['date_posted'] = datetime.utcnow()
+
+        db.add_post(post)
+        return redirect( url_for('home') )
+    return render_template('create_post.html', logged_in=session.get('username'))
+    
+
+# Route for veiwing the post
+@app.route('/post/<post_id>')
+@login_required
+def post(post_id):
+    post = db.get_post(post_id)
+
+    return render_template('post.html', post=post, logged_in=session.get('username'))
+
+
+# Route for editing the post
+@app.route('/post/<string:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = db.get_post(post_id)
+    if session.get('username') != post['author']['username']:
+        abort(403)
+    
+    if request.method == 'POST':
+        post['title'] = request.form.get('title')
+        post['content'] = request.form.get('content')
+
+        db.update_post(post)
+        return redirect( url_for('post', post_id=post_id))
+    return render_template('update_post.html', logged_in=session.get('username'), post=post)
+
+# Route for deleting a single post.
+@app.route('/post/<string:post_id>/delete', methods=['GET', 'POST'])
+@login_required
+def delete_post(post_id):
+    post = db.get_post(post_id)
+    if session.get('username') != post['author']['username']:
+        abort(403)
+    db.delete_post(post)
+    return redirect( url_for('home') )
